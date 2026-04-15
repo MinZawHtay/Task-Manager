034701baf7d2c3032450fffbff2878cb66f0121a
minzawhtay-umgodoodev18 <umgodoodev18@gmail.com>
2026-04-15 19:06:24 +0630

    [IMP]:upload github

--- TaskManager/TaskManager/asgi.py
+++ TaskManager/TaskManager/asgi.py
@@ -8,8 +8,8 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import Tasks.routing
 
